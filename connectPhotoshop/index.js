87be5175fdb63d2c8e2b86777c6407e34f2e7993
ZhangRui <skyperson@gmail.com>
2015-08-31 23:48:48 +0800

    update

--- connectPhotoshop/index.js
+++ connectPhotoshop/index.js
@@ -1,24 +1,74 @@
 /**
  * Created by zhangrui on 8/9/15.
  */
-var net = require('net');
+var Q = require("q"),
+    photoshop = require('./lib/photoshop');
 
-//通过端口创建客户端
-var client = net.connect({
-    host: '127.0.0.1',
-    port: '49494'
-}, function() {
-    console.log('已连接到服务器');
-    client.write('2lala.cn!');
-});
+var PHOTOSHOP_EVENT_PREFIX = "PHOTOSHOP-EVENT-";
 
-//data事件监听。收到数据后，断开连接
-client.on('data', function(data) {
-    console.log(data.toString());
-    client.end();
-});
+function Connect(){
+    if (!(this instanceof Connect)) {
+        return new Connect();
+    }
+    this._photoshop = null;
+    this._messageDeferreds = [];
+}
 
-//end事件监听，断开连接时会被触发
-client.on('end', function() {
-    console.log('已与服务器断开连接');
-});
+Connect.prototype.start = function (options) {
+    var self = this;
+
+    options = options || {};
+    self._options = options;
+    self._config = options.config || {};
+
+    function connectToPhotoshop() {
+        var connectionDeferred = Q.defer();
+        self._photoshop = photoshop.createClient(options);
+        self._photoshop.once("connect", function () {
+            connectionDeferred.resolve(self);
+        });
+
+        self._photoshop.on("close", function () {
+            console.log("Photoshop connection closed");
+            self.emit("close");
+        });
+
+        self._photoshop.on("error", function (err) {
+            console.log("Photoshop error", err);
+            // If the error does refers to a specific command we ran, reject the corresponding deferred
+            if (err && self._messageDeferreds.hasOwnProperty(err.id)) {
+                self._messageDeferreds[err.id].reject(err.body);
+            }
+            // TODO: Otherwise, gracefully shut down?
+        });
+
+        self._photoshop.on("communicationsError", function (err, rawMessage) {
+            console.log("photoshop communications error: %j", {error: err, rawMessage: rawMessage});
+        });
+
+        self._photoshop.on("message", function (messageID, parsedValue) { // ,rawMessage)
+            debugger;
+            if (self._messageDeferreds[messageID]) {
+                self._messageDeferreds[messageID].notify({type: "javascript", value: parsedValue});
+            }
+        });
+
+        self._photoshop.on("info", function (info) {
+            console.log("Photoshop info: %j", info);
+        });
+
+        self._photoshop.on("event", function (messageID, eventName, parsedValue) { // , rawMessage)
+            debugger;
+            self.emitPhotoshopEvent(eventName, parsedValue);
+        });
+
+        self._photoshop.on("pixmap", function (messageID, messageBody) { // , rawMessage)
+            debugger;
+            if (self._messageDeferreds[messageID]) {
+                self._messageDeferreds[messageID].notify({type: "pixmap", value: messageBody});
+            }
+        });
+
+        //return connectionDeferred.promise;
+    }
+};
