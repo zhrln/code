4891822913e3f01afe567a73735698ec18df2a22
ZhangRui <skyperson@gmail.com>
2015-09-22 18:00:38 +0800

    更新一点东西

--- lrSocket/src/asset/base/script/config.js
+++ lrSocket/src/asset/base/script/config.js
@@ -4,10 +4,15 @@
 (function(){
     var base = '/asset/base';
     require.config({
+        deps: ['flexible'],
         paths: {
+            // 本地
             'zepto': base + '/script/zepto',
             'connect': base + '/script/connect',
-            'socketio': '/socket.io/socket.io'
-        }
+            'socketio': '/socket.io/socket.io',
+            // 远端
+            'flexible': '//g.tbcdn.cn/mtb/lib-flexible/0.3.2/flexible'
+        },
+        urlArgs: 'v=20150922'
     });
 }());
